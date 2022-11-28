a172a08c9a06d8b4b6d87077e216e9231e096859
BenyBoy84 <Blais_Benoit@hotmail.com>
2022-11-28 14:56:00 -0500

    Add terraform fmt to GitHub Action test v1

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-  required_version = ">= 1.1.0"
+required_providers {
+random = {
+source  = "hashicorp/random"
+version = "3.0.1"
+}
+}
+required_version = ">= 1.1.0"
 }
