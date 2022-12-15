b3fcfd4c29ab9304217fff0c3dc5ce0169433b3d
BenyBoy84 <Blais_Benoit@hotmail.com>
2022-12-15 15:16:16 -0500

    change address range

--- variables.tf
+++ variables.tf
@@ -14,12 +14,12 @@ variable "location" {
 
 variable "vnet_cidr_range" {
   type    = string
-  default = "10.0.0.0/16"
+  default = "10.2.0.0/16"
 }
 
 variable "subnet_prefixes" {
   type    = list(string)
-  default = ["10.0.0.0/24", "10.0.1.0/24"]
+  default = ["10.2.0.0/24", "10.2.1.0/24"]
 }
 
 variable "subnet_names" {
