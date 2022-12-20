1c22083396ead383a4a466293e28843dffd27acd
BenyBoy84 <Blais_Benoit@hotmail.com>
2022-12-20 10:10:55 -0500

    fix issue

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #############################################################################
 
 terraform {
-required_providers {
+  required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.0"
@@ -31,7 +31,7 @@ provider "azurerm" {
 #############################################################################
 
 resource "azurerm_resource_group" "vnet_main" {
-  name     = var.resource_groupname
+  name     = var.resource_group_name
   location = var.location
 }
 
