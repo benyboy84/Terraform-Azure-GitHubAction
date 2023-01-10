e3d059b376c39124f36ca83e7ea46db77726ae7b
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-01-10 07:56:10 -0500

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 #############################################################################
-# TERRAFORM CONFIG 94
+# TERRAFORM CONFIG 95
 #############################################################################
 
 terraform {
@@ -38,7 +38,7 @@ resource "azurerm_resource_group" "vnet_main" {
 module "vnet-main" {
   source              = "Azure/vnet/azurerm"
   version             = "~> 2.0"
-  resource_group_name = azurerm_resource_group.vnet_main.name
+  resource_group_name = azurerm_resource_group.vnetmain.name
   vnet_name           = var.resource_group_name
   address_space       = [var.vnet_cidr_range]
   subnet_prefixes     = var.subnet_prefixes
